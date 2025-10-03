39201cb7b42540fcfd0a343804123e62dcce7a87
AaronJohnson <aaron@johnson.in>
2025-10-03 22:29:18 +0530

    Added Logistic Regression

--- CommML/Linear_Models.py
+++ CommML/Linear_Models.py
@@ -1,3 +1,5 @@
+import math
+
 class linear_regression:
   def __init__(self):
     self.slope = []
@@ -36,3 +38,46 @@ class linear_regression:
 
   def predict(self, x):
     return sum([_x * _slope for _x, _slope in zip(x, self.slope)]) + self.intercept
+
+
+
+
+  
+class polynomial_regression:
+  def __init__(self):
+    self.weights = []
+    self.bias = 0
+    self.learning_rate = 1e-7  
+
+
+  def fit(self, x, y, epochs):
+    x_len = len(x)
+    number_of_features = len(x[0])
+    self.weights = [0.0] * number_of_features
+
+    for epoch in range(epochs):
+      derivatives = [0.0] * number_of_features
+      bias_derivative = 0.0
+
+      for pos in range(x_len):
+        prediction = sum([self.weights[i] * (x[pos][i] ** (i + 1)) for i in range(number_of_features)]) + self.bias
+
+        for i in range(number_of_features):
+          derivatives[i] += (-2/x_len) * (x[pos][i] ** (i + 1)) * (y[pos] - prediction)
+
+        bias_derivative += (-2/x_len) * (y[pos] - prediction)
+
+      print(f"Epochs: {epoch} | Weights: {self.weights}")
+      for i in range(number_of_features):
+        self.weights[i] -= self.learning_rate * (derivatives[i] / x_len)
+
+      self.bias -= self.learning_rate * (bias_derivative / x_len)
+      if any(math.isnan(w) or math.isinf(w) or w >1e10 for w in self.weights):
+        print("Weights exploding or invalid, terminating training.")
+        return
+
+
+  def predict(self, x):
+    return sum([self.weights[i] * (x[0][i] ** (i + 1)) for i in range(len(self.weights))]) + self.bias
+
+
