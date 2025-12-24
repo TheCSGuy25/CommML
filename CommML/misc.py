db5a1857d947907fb847c284710ce8272d676756
Aaron <87066683+TheCSGuy25@users.noreply.github.com>
2025-12-24 10:43:15 +0530

    Bit of refactoring

--- CommML/misc.py
+++ CommML/misc.py
@@ -69,21 +69,20 @@ def min_max_scaler(x):
     """
 
     number_of_features = len(x[0]) if type(x[0]) == list else 1
-    if number_of_features == 1:
-        return [(x[i] - min(x)) / (max(x) - min(x)) for i in range((len(x)))]
-    else:
-        minimum = [min(x[i][j] for i in range(len(x))) for j in range(number_of_features)]
-        maximum = [max(x[i][j] for i in range(len(x))) for j in range(number_of_features)]
+    x = np.array(x)
 
-        if any(maximum[i] == minimum[i] for i in range(number_of_features)):
+    if number_of_features == 1:
+        minmum_x = min(x)
+        maximum_x = max(x)
+        return ((x - minmum_x) / (maximum_x - minmum_x)).tolist()
+    else:
+        min_x = np.min(x, axis=0)
+        max_x = np.max(x, axis=0)
+
+        if any(max_x[i] == min_x[i] for i in range(number_of_features)):
             raise ValueError("A minumum and maxium value is the same which leads to division by Zero")
         
-        return [
-                [(x[i][j] - minimum[j]) / (maximum[j] - minimum[j])
-                for j in range(number_of_features)]
-                for i in range(len(x))
-            ]
-
+        return ((x - min_x) / (max_x - min_x)).tolist()
 
 def mean_scaler(x):
     number_of_features  = len(x[0]) if type(x[0]) == list else 1
