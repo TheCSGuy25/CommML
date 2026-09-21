9b4cab00801d0523e889aaf3844bee9e195e0df8
TheCSGuy25 <87066683+TheCSGuy25@users.noreply.github.com>
2026-09-21 18:24:06 +0530

    Added Polynomial Regression

--- CommML/linear_models.py
+++ CommML/linear_models.py
@@ -1,4 +1,5 @@
 import numpy as np
+from itertools import combinations_with_replacement
 
 class LinearRegression:
     def __init__(self):
@@ -14,3 +15,32 @@ class LinearRegression:
     def predict(self, X):
         X = np.asarray(X, dtype=float)
         return X @ self.weights
+
+class PolynomialRegression:
+    def __init__(self, degree=2):
+        self.degree = degree
+        self.weights = None
+
+    def _transform(self, X):
+        X = np.asarray(X, dtype=float)
+        n_samples, n_features = X.shape
+        columns = [np.ones(n_samples)]
+        for degree in range(1, self.degree + 1):
+            for feature_indices in combinations_with_replacement(range(n_features), degree):
+                columns.append(np.prod(X[:, feature_indices], axis=1))
+
+        return np.column_stack(columns)
+
+    
+    def fit(self, X, y):
+        y = np.array(y)
+        X_poly = self._transform(X)
+        U, S, Vt = np.linalg.svd(X_poly, full_matrices=False)
+        s_inverse = np.array([ 1 / value if value >= 1e-10 else 0 for value in S ])
+        self.weights = Vt.T @ np.diag(s_inverse) @ U.T  @ y
+
+        return self
+
+    def predict(self, X):
+        return self._transform(X) @ self.weights
+
